6bceda2f729885b54556eefea92e66c825ff8dc3
Heixier <rzsiah@gmail.com>
2024-09-13 16:36:02 +0800

    added printf

---
+++ printf/pf_puthex.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pf_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsiah <rsiah@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 17:10:12 by rsiah             #+#    #+#             */
+/*   Updated: 2024/06/19 17:10:13 by rsiah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	count_hexa(unsigned int n)
+{
+	int	count;
+
+	count = 1;
+	while (n >= 16)
+	{
+		count++;
+		n /= 16;
+	}
+	return (count);
+}
+
+static void	write_in_hex(unsigned int hexa, int upper)
+{
+	if (upper)
+	{
+		if (hexa >= 16)
+			write_in_hex(hexa / 16, upper);
+		pf_putchar("0123456789ABCDEF"[hexa % 16]);
+	}
+	else
+	{
+		if (hexa >= 16)
+			write_in_hex(hexa / 16, upper);
+		pf_putchar("0123456789abcdef"[hexa % 16]);
+	}
+}
+
+int	pf_puthex(unsigned int hexa, int upper)
+{
+	int	count;
+
+	count = 0;
+	write_in_hex(hexa, upper);
+	count += count_hexa(hexa);
+	return (count);
+}
+
+// int main(int argc, char **argv)
+// {
+// 	(void)argc;
+// 	int my_res = pf_puthex(atoi(argv[1]), atoi(argv[2]));
+// 	int actl_res;
+// 	printf("\nMy count: %d\n", my_res);
+// 	if (atoi(argv[2]))
+// 		actl_res = printf("%X", atoi(argv[1]));
+// 	else
+// 		actl_res = printf("%x", atoi(argv[1]));
+// 	printf("\nActl count: %d\n", actl_res);
+// 	return (0);
+// }
