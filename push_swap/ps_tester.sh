aa12b1ff782e43f0a2a7897bebd20ca4426c35be
Heixier <rzsiah@gmail.com>
2024-10-08 03:09:40 +0800

    add valgrind option and progress bar

--- push_swap/ps_tester.sh
+++ push_swap/ps_tester.sh
@@ -2,10 +2,15 @@
 
 CHECKS=250
 SIZE=100
+NAME="ps_gen"
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]
 	then
-		echo  -e "Usage: \t $0 size_of_checks num_of_checks\n\nTests push_swap against random numbersCurrent defaults (internal):\n$CHECKS tests with $SIZE random numbers each."
+		echo  -e "Usage: $0 size times FLAGS\nGenerates random numbers for push_swap\n\n\
+OPTIONS (note: flags go at the back!):\n\n\
+	v-, --valgrind		checks for memory leaks (warning: takes more time!)\n
+Example: ./$NAME 100 250 -v\n
+Default: Tests $SIZE numbers $CHECKS times."
 		exit 0
 fi
 
@@ -31,15 +36,39 @@ if [ -n "$2" ]
 		fi
 fi
 
-RESULT=()
+MEMTEST=false
+if [[ "$3" == "--valgrind" ]] || [[ "$3" == "-v" ]]
+	then
+		MEMTEST=true
+fi
 
-for i in $(seq 1 $CHECKS);
+RESULT=() # Initialise RESULT as an array
+tput sc
+for i in $(seq 1 "$CHECKS");
 do
-	ARGS=$(shuf -i 0-2147483648 -n $SIZE | while read line; do echo "$((line * (1 - 2 * (RANDOM % 2))))"; done)
-	RESULT+=$(./push_swap $ARGS | wc -l)
-	RESULT+=' '
+	ARGS=$(shuf -i 0-2147483648 -n "$SIZE" | while read line; do echo "$((line * (1 - 2 * (RANDOM % 2))))"; done | tr '\n' ' ') # "random" because seed is fixed
+	if $MEMTEST
+		then
+			echo -en "$(tput rc; tput el)"
+			echo -en "Processing... $i/$CHECKS (note: valgrind can take a while)"
+			out=$(valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./push_swap "$ARGS" 2>&1) # Redirect stderr to stdout
+			if echo "$out" | grep -E "definitely lost|indirectly lost|possibly lost|ERROR SUMMARY: [^0]|Invalid" > /dev/null
+				then
+					echo -e "\n\e[1;91m$out\e[0m"
+					exit 1
+			fi
+		else
+			echo -en "\r\e[KProcessing... $i/$CHECKS"
+			out="$ARGS"
+    fi
+
+	count=$(echo "$out" | grep -vc "==")
+	RESULT+=("$count")
 done
 
-printf "%s\n" ${RESULT[@]} | sort -n | \
-awk -v size="$SIZE" -v checks="$CHECKS" 'NR==1 {min = $1} END {printf("\\e[1;92mTest complete!\\e[0m\\nTimes tested: %d | Size tested: %d\\nMax: %d | Min: %d", checks, size, $1, min)}' | \
-echo -e $(cat)
+echo -en "$(tput rc; tput el)"
+
+printf "%s\n" "${RESULT[@]}" | sort -n | \
+awk -v size="$SIZE" -v checks="$CHECKS" 'NR==1 {min = $1} END \
+{printf("\\e[1;92mTest complete!\\e[0m\\nSize: %d | Times tested: %d\\nMax: %d | Min: %d", size, checks, $1, min)}' | \
+echo -e "$(cat)"
