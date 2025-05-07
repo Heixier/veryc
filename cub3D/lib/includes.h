14224979a6807d844987026f317c5b485c1be507
Heixier <rzsiah@gmail.com>
2025-05-07 11:28:27 +0800

    init more files

---
+++ cub3D/lib/includes.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsiah <rsiah@42singapore.sg>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 17:36:08 by rsiah             #+#    #+#             */
+/*   Updated: 2025/05/06 18:00:11 by rsiah            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDES_H
+# define INCLUDES_H
+
+// Files and memory (open, close, read)
+#include <fcntl.h>
+#include <unistd.h>
+
+// Input and outputs
+#include <stdlib.h>
+#include <stdio.h>
+
+// Timing and calculations
+#include <time.h>
+#include <math.h>
+
+#endif
